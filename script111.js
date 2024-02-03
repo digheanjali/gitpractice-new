eb4ab79928e685b91fa8f762ca1be3b228ceb2c9
digheanjali <anjalipawase1999@gmail.com>
2024-02-02 19:11:42 -0800

    login

--- script111.js
+++ script111.js
@@ -1,1 +1,18 @@
 console.log("hello anjali")
+
+
+
+
+// First two commands to execute only one
+// Set your username: git config --global user.name "username"
+// Set your email address: git config --global user.email "useremail@gmail.com"
+
+// git init
+// git add .
+// git commit -m "login"
+
+// // Sign in to your git hub , Create new repo and run last 3 commands
+// git branch -M main
+// git remote add origin https://github.com/minskolecourse/python.git
+// git push -u origin main
+// one time username and wil be asked
